3f3eb9993cee4e1e8716e191366f5db7824dbd66
alex_zhooson <861077046@qq.com>
2021-03-10 13:37:59 +0800

    docs: 更新文档

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # vue3-cnode
 
+## 访问地址
+
+[demo 访问](http://cnode.zhooson.cn/#/index)
+
 ## Project setup
 
 ```
@@ -13,3 +17,20 @@ yarn build
 ## vue3 的文档
 
 [文档入口](https://www.vue3js.cn/docs/zh/)
+
+## nginx.conf
+
+```
+    server {
+        listen       80;
+        server_name  cnode.zhooson.cn;
+        access_log off;
+
+        location / {
+            root   /workspace/cnodejs/;
+            index index.html index.htm;
+		    try_files $uri $uri/ /index.html;
+         }
+    }
+
+```
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 import Home from "../views/home/home.vue";
 import store from "../store";
 
@@ -8,52 +12,53 @@ const routes = [
     name: "home",
     component: Home,
     redirect: {
-      path: "/index"
+      path: "/index",
     },
     children: [
       {
         path: "/index",
         name: "index",
-        component: () => import("../views/index/index")
+        component: () => import("../views/index/index"),
       },
 
       {
         path: "/post",
         name: "post",
-        component: () => import("../views/post/post")
+        component: () => import("../views/post/post"),
       },
       {
         path: "/my",
         name: "my",
-        component: () => import("../views/my/my")
-      }
-    ]
+        component: () => import("../views/my/my"),
+      },
+    ],
   },
   {
     path: "/detail",
     name: "detail",
-    component: () => import("../views/detail/detail")
+    component: () => import("../views/detail/detail"),
   },
   {
     path: "/token",
     name: "token",
-    component: () => import("../views/my/token")
+    component: () => import("../views/my/token"),
   },
   {
     path: "/collect",
     name: "collect",
-    component: () => import("../views/my/collect")
+    component: () => import("../views/my/collect"),
   },
   {
     path: "/theme",
     name: "theme",
-    component: () => import("../views/my/theme")
-  }
+    component: () => import("../views/my/theme"),
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  // history: createWebHistory(),
+  history: createWebHashHistory(),
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
